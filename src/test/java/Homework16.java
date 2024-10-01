aa066e567c8ee249db56314dbfe723d6c33d27c7
nkalinina73 <35932117+nkalinina73@users.noreply.github.com>
2024-10-01 11:53:42 -0700

    NK HW-16

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -6,11 +8,12 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
-    @Test
-    public void loginEmptyEmailPassword() {
+public class Homework16 extends BaseTest{
 
-//      Added ChromeOptions argument below to fix websocket error
+    @Test
+    public void registrationNavigation(){
+
+        //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
@@ -18,9 +21,14 @@ public class LoginTests extends BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://qa.koel.app/";
-        //driver.get(url);
-        //Assert.assertEquals(driver.getCurrentUrl(), url);
-       // driver.quit();
-        //cloned Oct
-        }
+        driver.get(url);
+
+        WebElement registrationLink = driver.findElement(By.cssSelector("a[href='registration']"));
+        registrationLink.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://qa.koel.app/registration");
+
+
+    }
+
 }
