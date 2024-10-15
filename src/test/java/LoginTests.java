61f8c04753c76cacbf174c5f91fff75c72d45b8e
nkalinina73 <35932117+nkalinina73@users.noreply.github.com>
2024-10-15 11:16:08 -0700

    LoginTests updated
    Created/updated BasePage, LoginPage and HomePage.
    
    Edited the LoginTests for the POM framework

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pom.HomePage;
+import pom.LoginPage;
 
 import java.time.Duration;
 
@@ -10,8 +12,18 @@ public class LoginTests extends BaseTest {
     @Test
     public void loginEmptyEmailPassword() {
 
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+
+        loginPage.provideEmail("natalia.kalinina@testpro.io");
+        loginPage.providePassword("nkKoel24$");
+        loginPage.clickSubmit();
+
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+
+// ===================================================================================
 //      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
+    /*    ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
@@ -20,7 +32,10 @@ public class LoginTests extends BaseTest {
         String url = "https://app.testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        driver.quit();*/
+        //=====================================================
+
+
 
     }
 }
